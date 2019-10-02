24c8e50ecf8c27859bfa4850ddb9033cc6bbd82b
Ramzibawayza <ramzibawayza12@gmail.com>
2019-10-02 03:23:08 +0300

    edit

--- Java.js
+++
@@ -1,9 +0,0 @@
-var a=confirm("DO YOU LIKE THIS PAGE")
-      if(a==yes) {
-        greeting = 'ENJOY!';
-      } else {
-        greeting = 'TRY THIS';
-      }
-      document.write('<p>' + greeting + '</p>');
-    
-    }
